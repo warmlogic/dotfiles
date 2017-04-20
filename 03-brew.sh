3b4e59fef8e8d2b4038f68ff3416023b96b48aba
Matt Mollison <matt.mollison@gmail.com>
2017-04-20 13:49:01 -0700

    use Brewfile

--- 03-brew.sh
+++ 03-brew.sh
@@ -2,30 +2,25 @@
 
 # Install command-line tools using Homebrew.
 
-# Make sure we’re using the latest Homebrew.
-brew update
+# Ask for the administrator password upfront.
+sudo -v
 
-# Upgrade any already-installed formulae.
-brew upgrade
+# Get homebrew
+# Modified from: https://github.com/pathikrit/mac-setup-script/blob/master/setup.sh
+if test ! $(which brew); then
+  echo "Installing Xcode ..."
+  xcode-select --install
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
+  echo "Installing Homebrew ..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Updating Homebrew ..."
+  brew update
+  brew upgrade
+fi
 
-# # Install some other useful utilities like `sponge`.
-# brew install moreutils
-
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
+# This installs the Brewfile
+brew bundle
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
@@ -33,75 +28,5 @@ if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
   chsh -s /usr/local/bin/bash;
 fi;
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-# brew install ringojs
-# brew install narwhal
-
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install homebrew/dupes/grep
-# brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-# brew install homebrew/php/php56 --with-gmp
-
-# # Install font tools.
-# brew tap bramstein/webfonttools
-# brew install sfnt2woff
-# brew install sfnt2woff-zopfli
-# brew install woff2
-
-# # Install some CTF tools; see https://github.com/ctfs/write-ups.
-# brew install aircrack-ng
-# brew install bfg
-# brew install binutils
-# brew install binwalk
-# brew install cifer
-# brew install dex2jar
-# brew install dns2tcp
-# brew install fcrackzip
-# brew install foremost
-# brew install hashpump
-# brew install hydra
-# brew install john
-# brew install knock
-# brew install netpbm
-# brew install nmap
-# brew install pngcheck
-# brew install socat
-# brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-# brew install xpdf
-# brew install xz
-
-# # Install other useful binaries.
-# brew install ack
-# brew install dark-mode
-# #brew install exiv2
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-# brew install lua
-# brew install lynx
-# brew install p7zip
-# brew install pigz
-# brew install pv
-# brew install rename
-# brew install rhino
-# brew install speedtest_cli
-# brew install ssh-copy-id
-# brew install testssl
-brew install tree
-# brew install vbindiff
-# brew install webkit2png
-# brew install zopfli
-
-# Remove outdated versions from the cellar.
+# Remove outdated versions from the cellar
 brew cleanup
---
+++ Brewfile
@@ -0,0 +1,140 @@
+# Install via
+# $ brew tap Homebrew/bundle
+# $ brew bundle
+
+# This puts the applications in the correct folder
+cask_args appdir: '/Applications'
+
+# List of brew repositories
+tap 'homebrew/bundle'
+tap 'homebrew/dupes'
+tap 'caskroom/versions'
+tap 'homebrew/versions'
+
+# Use GNU instead of BSD commands
+# the with-default-names uses normal name, rather than
+brew 'coreutils'
+# brew 'binutils'
+# brew 'diffutils'
+brew 'findutils', args: ['with-default-names']
+brew 'gawk'
+brew 'gnu-indent', args: ['with-default-names']
+brew 'gnu-sed', args: ['with-default-names']
+brew 'gnu-tar', args: ['with-default-names']
+brew 'gnu-which', args: ['with-default-names']
+brew 'gnutls', args: ['with-default-names']
+brew 'grep', args: ['with-default-names']
+brew 'gzip'
+brew 'screen'
+brew 'watch'
+brew 'wdiff', args: ['with-gettext']
+brew 'wget', args: ['with-iri']
+
+# Update tools that already exist on Mac
+
+# Newest version of Bash 4.x
+brew 'bash'
+# Bash tab completion for Bash 4.x
+brew 'bash-completion2'
+# Better top command
+brew 'htop'
+
+# Git and some surrounds
+brew 'git'
+brew 'git-lfs'
+brew 'git-flow'
+brew 'git-extras'
+# brew 'hub'
+
+# Amazon command line interface
+brew 'awscli'
+
+# Simple cheat-sheets; install and run $ cheat tar
+brew 'cheat'
+
+# brew 'ctags'
+brew 'file-formula'
+brew 'gcc'
+brew 'gpatch'
+brew 'gpg'
+# brew 'jsonpp'
+brew 'less'
+brew 'make'
+# brew 'most'
+brew 'rsync'
+# # Don't use non-native ssh if storing passphrase in Keychain
+# brew 'openssh'
+# brew 'ssh-copy-id'
+brew 'imagemagick', args: ['with-webp']
+
+brew 'pandoc'
+brew 'rename'
+# brew 'svn'
+# brew 'the_silver_searcher'
+# Using tmux
+brew 'tmux'
+# This apparently helps tmux work without problems.
+brew 'reattach-to-user-namespace'
+
+# Visualize directory structure
+brew 'tree'
+
+brew 'unrar'
+brew 'unzip'
+brew 'vim', args: ['override-system-vi']
+brew 'emacs', args: ['--with-cocoa']
+
+# Mac Swiss Army Knife https://github.com/rgcr/m-cli
+brew 'm-cli'
+
+# Install additional applications
+
+cask 'google-cloud-sdk'
+cask 'google-chrome'
+# cask 'sublime-text'
+cask 'sublime-text-dev'
+cask 'aquamacs'
+cask 'github-desktop'
+cask 'dropbox'
+cask 'arq'
+cask 'keybase'
+cask 'caffeine'
+cask 'spectacle'
+cask 'quicksilver'
+cask 'yujitach-menumeters'
+cask 'itsycal'
+cask 'omnidisksweeper'
+cask 'harvest'
+cask 'toggldesktop'
+cask 'selfcontrol'
+cask 'transmission'
+cask 'vlc'
+
+# Install QuickLook plugins https://github.com/sindresorhus/quick-look-plugins
+
+cask 'qlcolorcode'
+cask 'qlstephen'
+cask 'qlmarkdown'
+cask 'quicklook-json'
+cask 'qlprettypatch'
+cask 'quicklook-csv'
+cask 'betterzipql'
+cask 'qlimagesize'
+cask 'webpquicklook'
+cask 'suspicious-package'
+cask 'quicklookase'
+cask 'qlvideo'
+
+# # Not installing these applications via brew cask
+
+# cask 'slack'
+# cask 'flux'
+# cask 'sourcetree'
+# cask 'spotify'
+# cask 'spotify-notifications'
+# cask 'iterm2'
+# cask 'adobe-reader'
+
+# # This installs helper functions.
+# tap 'jbwhit/tap'
+# brew 'nb3automation'
