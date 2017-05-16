f129df3023c5eab1b7c4a54d4ff271327b5c541f
Matt Mollison <matt.mollison@gmail.com>
2017-05-16 12:29:33 -0700

    spaces to tabs

--- 01-bootstrap.sh
+++ 01-bootstrap.sh
@@ -5,27 +5,27 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
+    rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
         --exclude ".extra" \
         --exclude "01-bootstrap.sh" \
         --exclude "02-brew.sh" \
         --exclude "03-macos.sh" \
         --exclude "04-python.sh" \
         --exclude "Brewfile" \
-		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+        --exclude "README.md" \
+        --exclude "LICENSE-MIT.txt" \
+        -avh --no-perms . ~;
+    source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+    doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
 fi;
 unset doIt;
