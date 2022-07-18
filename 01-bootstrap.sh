a7bd9f844c53af44e4509f437b3876d3e9e7be5a
Matt Mollison <matt.mollison@gmail.com>
2022-07-17 21:28:35 -0400

    simplify

--- 01-bootstrap.sh
+++ 01-bootstrap.sh
@@ -21,13 +21,9 @@ function doIt() {
     source ~/.zshrc;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     doIt;
-else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-    echo "";
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt;
-    fi;
 fi;
 unset doIt;
