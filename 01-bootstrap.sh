554dfeb5772db844c0283e236d1dfcf9fb7ba153
Matt Mollison <matt.mollison@gmail.com>
2026-09-09 16:36:24 -0400

    Move home-mapped files under home/ and drop rsync exclude list

--- 01-bootstrap.sh
+++ 01-bootstrap.sh
@@ -8,19 +8,7 @@ git pull origin $(git rev-parse --abbrev-ref HEAD);
 MY_SHELL=$(ps -p $$ -ocomm=)
 
 function doIt() {
-    rsync --exclude ".git/" \
-        --exclude ".DS_Store" \
-        --exclude ".extra" \
-        --exclude "01-bootstrap.sh" \
-        --exclude "02-brew.sh" \
-        --exclude "03-macos.sh" \
-        --exclude "04-python.sh" \
-        --exclude "05-python-updates.sh" \
-        --exclude "Brewfile" \
-        --exclude "Brewfile_work" \
-        --exclude "README.md" \
-        --exclude "LICENSE-MIT.txt" \
-        -avh --no-perms . ~;
+    rsync -avh --no-perms --exclude .DS_Store home/ ~;
     if [[ "$MY_SHELL" =~ 'zsh' ]]; then
         if [ -f ~/.zshrc ]; then
             source ~/.zshrc;
